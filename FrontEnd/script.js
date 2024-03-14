95a88f0e9f47c4382dbb0f7430d605496fa6902b
AlmostH <30635016+AlmostH@users.noreply.github.com>
2024-03-14 12:23:05 +0100

    Ajout des travaux recuperé sur l'Api avec javascript

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,10 +1,31 @@
+
+
+/* Recuperer les travaux sur l'API*/
+
 async function getWorks() {
     let response = await fetch("http://localhost:5678/api/works");
     return await response.json();
   }
-(async () => {  
+/*(async () => {  
 console.log(await getWorks());
-})()
+})()*/
 
+/* Afficher les travaux*/
 
-  
+const gallery = document.getElementsByClassName("gallery")[0];
+
+async function showWorks(){
+    const galleryWorks = await getWorks();
+    //console.log(galleryWorks);
+    galleryWorks.forEach((work) => {
+        const figure = document.createElement("figure");
+        const image = document.createElement("img");
+        image.src = work.imageUrl;
+        const figcaption = document.createElement("figcaption");
+        figcaption.textContent= work.title;
+        figure.appendChild(image);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+    });
+}
+showWorks();
